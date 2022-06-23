5a2705d6b39efbac8008fcc29e88e9c781a728b3
Hari Pandey <69706767+Harry-code21@users.noreply.github.com>
2022-06-23 16:17:23 +0545

    explore music added

---
+++ pages/explorenfts/index.js
@@ -0,0 +1,22 @@
+/* eslint-disable react/no-unescaped-entities */
+
+
+
+
+export default function index() {
+    
+
+
+
+
+
+    return (
+       
+        <>
+        EXPLORE Nfts 
+      </>
+
+        
+    );
+}
+
--- pages/index.js
+++ pages/index.js
@@ -68,10 +68,10 @@ function index() {
           <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">Music acts like a magic key, to which the most tightly closed heart opens.</p>
           <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
           <div class="rounded-md shadow">
-              <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"> Explore Music </a>
+              <a href="./exploremusic" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"> Explore Music </a>
             </div>
             <div class="mt-3 sm:mt-0 sm:ml-3">
-              <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">Explore NFTs </a>
+              <a href="./explorenfts" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">Explore NFTs </a>
             </div>
             
            
